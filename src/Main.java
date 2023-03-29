d7876210fc9feaea186da2e0b510f642efc43ad5
Mlakar na macu <timo.mlakar@gmail.com>
2023-03-29 13:19:46 +0200

    Added basic frame functionality

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        //todo stuff comes here
+        Frame frame = new Frame();
+        frame.pack();
+        frame.setVisible(true);
     }
 }
