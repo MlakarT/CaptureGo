a7ec76df8e6e8ea8f4e13bc7e09396dda0f6ebcd
Mlakar na macu <timo.mlakar@gmail.com>
2023-03-29 13:59:38 +0200

    some menu stuffs

--- src/Frame.java
+++ src/Frame.java
@@ -3,15 +3,35 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Frame extends JFrame implements ActionListener {
+    protected Panel panel;
+
     public Frame() {
         super();
         setTitle("Play Capture Go");
         //todo make dynamically scaling window
-        panel = new Panel(800,800);
+        Panel panel = new Panel(800,800);
         add(panel);
+
+        JMenuBar menubar = new JMenuBar();
+        setJMenuBar(menubar);
+
+        JMenu menuGame = addMenu(menubar, "Settings");
+
+
 
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    private JMenu addMenu(JMenuBar menubar, String title) {
+        JMenu menu = new JMenu(title);
+        menubar.add(menu);
+        return menu;
+    }
+    private JMenuItem addMenuItem(JMenu menu, String title){
+        JMenuItem menuitem = new JMenuItem(title);
+        menu.add(menuitem);
+        menuitem.addActionListener(this);
+        return menuitem;
     }
     /**
      * Invoked when an action occurs.
