d17b656161f88dc6307e3ac8aa761de346754919
tej-boop <82840166+tej-boop@users.noreply.github.com>
2023-03-29 13:20:44 +0200

    panel started

--- src/Panel.java
+++ src/Panel.java
@@ -1,2 +1,47 @@
-public class Panel {
+import javax.swing.*;
+import java.awt.event.*;
+
+public class Panel extends JPanel implements MouseListener, MouseMotionListener{
+
+    public Panel() {
+        super();
+        addMouseListener(this);
+        addMouseMotionListener(this);
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+
+    }
 }
