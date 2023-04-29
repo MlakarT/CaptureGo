182ee4238f4e484a21af075c47cfeebf854faba8
tej-boop <82840166+tej-boop@users.noreply.github.com>
2023-04-29 15:15:18 +0200

    panel: panel finished?

--- src/gui/Panel.java
+++ src/gui/Panel.java
@@ -16,6 +16,9 @@ public class Panel extends JPanel implements MouseListener {
     protected Stroke gridWidth;
     protected Stroke playerOutlineWidth;
     protected int radius;
+    // a number that updates whenever a player makes a move. this helps determine
+    // the colour of a captured block:
+    protected int index;
 
     // light mode colors
     protected Color colorLightBackground;
@@ -48,9 +51,10 @@ public class Panel extends JPanel implements MouseListener {
         setFocusable(true);
 
         // startup values:
-        gridWidth = new BasicStroke(10);
-        playerOutlineWidth = new BasicStroke(10);
-        radius = 20;
+        gridWidth = new BasicStroke(2);
+        playerOutlineWidth = new BasicStroke(2);
+        radius = 40;
+        int index = 0;
 
         colorLightBackground = Color.LIGHT_GRAY;
         colorLightBoard = new Color(242,176,109,255);
@@ -59,7 +63,7 @@ public class Panel extends JPanel implements MouseListener {
         colorLightPlayerBlackOutline = Color.BLACK;
         colorLightPlayerWhite = Color.WHITE;
         colorLightPlayerWhiteOutline = Color.BLACK;
-        colorLightCapturedBlock = Color.YELLOW;
+        colorLightCapturedBlock = Color.RED;
 
         colorDarkBackground = Color.DARK_GRAY;
         colorDarkBoard = new Color(32,33,36,255);
@@ -90,61 +94,73 @@ public class Panel extends JPanel implements MouseListener {
         if (igra == null) return;
         int size = igra.size;
         int[][] grid = igra.grid;
+        Graphics2D g2 = (Graphics2D) g;
 
         // drawing the board:
-        int boardWidth = size * CELL_SIZE;
-        int boardHeight = size * CELL_SIZE;
-        int boardX = (getWidth() - boardWidth) / 2;
-        int boardY = (getHeight() - boardHeight) / 2;
+        int boardWidth = (size - 1) * CELL_SIZE;
+        int boardHeight = (size - 1) * CELL_SIZE;
+        int boardX = (getWidth() - boardWidth) / 2 - CELL_SIZE / 2;
+        int boardY = (getHeight() - boardHeight) / 2 - CELL_SIZE / 2;
         g.setColor(colorLightBoard);
         g.fillRect(boardX, boardY, boardWidth, boardHeight);
 
         // drawing the grid:
         g.setColor(colorLightGrid);
-        for (int i = 0; i <= size; i++) {
-            g.drawLine(boardX + i * CELL_SIZE, boardY, boardX + i * CELL_SIZE, boardY + size * CELL_SIZE);
-            g.drawLine(boardX, boardY + i * CELL_SIZE, boardX + size * CELL_SIZE, boardY + i * CELL_SIZE);
+        for (int i = 0; i < size; i++) {
+            g2.setStroke(gridWidth);
+            g.drawLine(boardX + i * CELL_SIZE, boardY, boardX + i * CELL_SIZE, boardY + (size - 1) * CELL_SIZE);
+            g.drawLine(boardX, boardY + i * CELL_SIZE, boardX + (size - 1) * CELL_SIZE, boardY + i * CELL_SIZE);
         }
 
         //drawing the stones:
         for (int row = 0; row < size; row++) {
             for (int col = 0; col < size; col++) {
                 int stone = grid[row][col];
+                g2.setStroke(playerOutlineWidth);
                 if (stone == Igra.BLACK_STATE) {
-                    g.setColor(colorLightPlayerBlack);
-                    g.fillOval(boardX + col * CELL_SIZE, boardY + row * CELL_SIZE, CELL_SIZE, CELL_SIZE);
-                    g.setColor(colorLightPlayerBlackOutline);
-                    g.drawOval(boardX + col * CELL_SIZE, boardY + row * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+                    drawStone(g, boardX, boardY, row, col, colorLightPlayerBlack, colorLightPlayerBlackOutline);
                 }
                 else if (stone == Igra.WHITE_STATE) {
-                    g.setColor(colorLightPlayerWhite);
-                    g.fillOval(boardX + col * CELL_SIZE, boardY + row * CELL_SIZE, CELL_SIZE, CELL_SIZE);
-                    g.setColor(colorLightPlayerWhiteOutline);
-                    g.drawOval(boardX + col * CELL_SIZE, boardY + row * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+                    drawStone(g, boardX, boardY, row, col, colorLightPlayerWhite, colorLightPlayerWhiteOutline);
                 }
                 else if (stone == Igra.CAPTURED_STATE) {
-                    //TODO captured state
+                    if (index % 2 == 0) {
+                        drawStone(g, boardX, boardY, row, col, colorLightPlayerBlack, colorLightCapturedBlock);
+                    }
+                    else drawStone(g, boardX, boardY, row, col, colorLightPlayerWhite, colorLightCapturedBlock);
                 }
             }
         }
+    }
+
+    private void drawStone(Graphics g, int boardX, int boardY, int row, int col, Color colorStone, Color colorStoneOutline) {
+        g.setColor(colorStone);
+        g.fillOval(boardX + col * CELL_SIZE - radius / 2, boardY + row * CELL_SIZE - radius / 2, radius, radius);
+        g.setColor(colorStoneOutline);
+        g.drawOval(boardX + col * CELL_SIZE - radius / 2, boardY + row * CELL_SIZE - radius / 2, radius, radius);
     }
 
     @Override
     public void mouseClicked(MouseEvent e) {
         if (igra == null) return;
         if (igra.gameOver()) return;
+        int x = e.getX();
+        int y = e.getY();
         int size = igra.size;
-        int boardWidth = size * CELL_SIZE;
-        int boardHeight = size * CELL_SIZE;
-        int boardX = (getWidth() - boardWidth) / 2;
-        int boardY = (getHeight() - boardHeight) / 2;
-        int x = (e.getX() - boardX) / CELL_SIZE;
-        int y = (e.getY() - boardY) / CELL_SIZE;
-        Poteza poteza = new Poteza(x,y);
+        int boardWidth = (size - 1) * CELL_SIZE;
+        int boardHeight = (size - 1) * CELL_SIZE;
+        int boardX = (getWidth() - boardWidth) / 2 - CELL_SIZE / 2;
+        int boardY = (getHeight() - boardHeight) / 2 - CELL_SIZE / 2;
+        int col = (x - boardX + CELL_SIZE / 2) / CELL_SIZE;
+        int row = (y - boardY + CELL_SIZE / 2) / CELL_SIZE;
+
+        Poteza poteza = new Poteza(col, row);
         boolean success = igra.odigraj(poteza);
         if (success) {
             igra.odigraj(poteza);
+            index++;
         }
+
         repaint();
     }
 
